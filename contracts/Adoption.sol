0035bba6f3a75dd8c8955a49f69a3ff00413ff17
Chris Farrugia <chris@joinblink.com>
2018-05-16 11:53:08 +0100

    add adopt function

--- contracts/Adoption.sol
+++ contracts/Adoption.sol
@@ -2,5 +2,18 @@ pragma solidity ^0.4.17;
 
 contract Adoption {
 
+    //an array of ethereum addresses storing the list of adopter addresses
+    address[16] public adopters; //public variables automatically come with getter methods
 
+    // adopt a pet by specifying the id of the pet to be adopted
+    function adopt(uint petId) public returns (uint) {
+
+      //check that the petId is within range - 16 pets in total)
+      require(petId >= 0 && petId <= 15);
+
+      //assign the address of the person or smart contract that called this function.
+      adopters[petId] = msg.sender;
+
+      return petId;
+    }
 }
